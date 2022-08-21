d37079628cd947faaa2ceee52e05ea946749c757
Gokulcasper <ronitcasper215@gmail.com>
2022-08-21 17:06:04 +0530

    Feat : creating -Iphone Store

--- src/components/CreateMobile.js
+++ src/components/CreateMobile.js
@@ -19,19 +19,19 @@ const CreateMobile = () => {
     return (
         <div className='flex flex-col items-center space-y-5 w-full h-full'>
             <div className='flex items-center justify-start gap-x-3'>
-                <h3>Name :</h3>
+                <h3>Name:</h3>
                 <input type="text" value={mobile.name} onChange={(e) => {
                     setMobile({ ...mobile, name: e.target.value })
                 }} />
             </div>
             <div className='flex items-center justify-start gap-x-3'>
-                <h3>Descriptions :</h3>
+                <h3>Descriptions:</h3>
                 <input type="text" value={mobile.description} onChange={(e) => {
                     setMobile({ ...mobile, description: e.target.value })
                 }} />
             </div>
             <div className='flex items-center justify-start gap-x-3'>
-                <h3>Image Url :</h3>
+                <h3>Image Url:</h3>
                 <input type="text" value={mobile.image_url} onChange={(e) => {
                     setMobile({ ...mobile, image_url: e.target.value })
                 }} />
